cac5a96e5eda3a7ebf312e7e2ef6d092535966fd
japanes <napososhok@gmail.com>
2020-06-01 16:48:16 +0300

    searchform markup

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Поиск">
-    <button class="search_btn">Найти</button>
-    <button class="search_close">Закрыть</button>
+    <button class="search_btn"></button>
 </form>
